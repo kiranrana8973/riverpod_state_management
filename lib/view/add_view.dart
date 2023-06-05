5a1448cf2866226f7f15d3526b1fe2eacc96d363
Kiran Rana <kiranrana8973@gmail.com>
2023-06-05 11:06:51 +0545

    Added arithmetic provider

--- lib/view/add_view.dart
+++ lib/view/add_view.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_state_management/logic/arithmetic.dart';
 
-class AddView extends StatelessWidget {
+class AddView extends ConsumerStatefulWidget {
   const AddView({super.key});
 
   @override
+  ConsumerState<AddView> createState() => _AddViewState();
+}
+
+class _AddViewState extends ConsumerState<AddView> {
+  final firstNoController = TextEditingController();
+  final secondNoController = TextEditingController();
+
+  int result = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add'),
+      ),
+      body: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'First number',
+            ),
+          ),
+          const SizedBox(height: 8),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Second number',
+            ),
+          ),
+          const SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: () {
+              int first = int.parse(firstNoController.text);
+              int second = int.parse(secondNoController.text);
+              var arithmetic = ref.read(arithmeticProvider);
+              setState(() {
+                result = arithmetic.add(first, second);
+              });
+            },
+            child: const Text('Add'),
+          ),
+          const SizedBox(height: 8),
+          Text('Result is: $result'),
+        ],
+      ),
+    );
   }
 }
--- lib/view/counter_view.dart
+++ lib/view/counter_view.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterView extends StatelessWidget {
+//Create a provider
+// Read only
+final nameProvider = Provider<String>((ref) => "Hello world");
+
+// WidgetRef : Widget -> Provider
+// ProviderRef : Provider -> Provider
+final numberProvider = StateProvider<int>((ref) {
+  return 100;
+});
+
+class CounterView extends ConsumerWidget {
   const CounterView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          '1',
-          style: TextStyle(
+          // Reading a provider
+          ref.watch(numberProvider).toString(),
+          style: const TextStyle(
             fontSize: 24,
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => ref.read(numberProvider.notifier).state++,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
